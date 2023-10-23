3fd1b6533350ce57677e2735a073ff1c428a2467
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-10-23 17:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function liveTime(){
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear();
+let time = date.get.toLocaleTimeString('en-US',{hour : 'nummeric' , minutes : 'numeric', second : 'numeric', hour12 : 'true'});
+let cudate = '${month}/{day}/{year}';
+	document.getElementById("timer").textContent = cuedate+','+time
+}
+function startTimer(){
+	liveTime();
+	setInterval(liveTime,1000)
+}
+
+window.addKeyEventListener('load',startTimer);
+
